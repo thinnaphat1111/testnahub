dad4615e5388c48d8c8329f04eab396662e64b60
thinnaphat1111 <68723105+thinnaphat1111@users.noreply.github.com>
2020-07-31 02:20:27 +0700

    grade

---
+++ .vscode/grade.py
@@ -0,0 +1,17 @@
+score = int (input())
+if  score < 0;
+    print("ERROR")
+elif score >= 50 and score <54;
+    print("D")
+elif score >= 55 and score <59;
+    print("D+")
+elif score >= 60 and score <64;
+    print("C")
+elif score >= 65 and score <69;
+    print("C+")
+elif score >= 70 and score <74;
+    print("B")
+elif score >= 75 and score <79;
+    print("B+")
+elif score >= 80 and score <=100;
+    print("A")
